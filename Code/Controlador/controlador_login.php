f9628184c4b651cf1b5e9e2ad36553dc0f631dae
CesarGarzonJametti <cagarzon3@espe.edu.ec>
2023-01-08 14:39:39 -0500

    Modify controlador_login.php

---
+++ Code/Controlador/controlador_login.php
@@ -0,0 +1,10 @@
+<?php
+    if(!empty($_POST["bt_iniciar_sesion"])) {
+        if(!empty($_POST["correo"]) and !empty($_POST["password"])) {
+            
+        }
+        else {
+            echo "Campos vacíos";
+        }
+    }
+?>
