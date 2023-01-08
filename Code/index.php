6b032dbf0237f61086e9b3e0c85eb1f6bd01f43d
CesarGarzonJametti <cagarzon3@espe.edu.ec>
2023-01-08 18:20:07 -0500

    Modify

--- Code/index.php
+++ Code/index.php
@@ -3,8 +3,6 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        
-        <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"> -->
         <link rel="stylesheet" href="../Code/css/principal.css" type="text/css" media="screen">
 
         <title>Saga Nutrition</title>
@@ -13,7 +11,7 @@
         <header>
             <img class="logo" src="../Code/img/logo2.jpg" alt="Saga Nutrition">
             <div class="nav_principal">
-                <a href="../Code/paginas/login.php">
+                <a class="lk_ico_nav_principal" href="../Code/paginas/login.php">
                     <img class="ico_nav_principal" src="../Code/img/usuario.jpg" alt="Saga Nutrition">
                 </a>
                 <img class="ico_nav_principal" src="../Code/img/carrito.jpg" alt="Saga Nutrition">
@@ -36,106 +34,5 @@
                 </div>
             </section>
         </main>
-
-
-
-
-        <!--<div class="head">
-            
-            <div class="logo">
-                
-                <img src="../Code/img/logo2.jpg" alt="logo2" style="width: 100;height: 70px;" >
-
-            </div>
-            <nav class="navbar">
-                <a href="#">Inicio</a>
-                <a href="#">Nosotros</a>
-                <a href="#">Contacto</a>
-                <a href="#">Productos</a>
-                <a href="#">Precio</a>
-                <i class="fab fa-"></i>
-
-            </nav>
-
-
-        </div>
-        <header class="content header">
-            <h2 class="title">Inicio</h2>
-            <img src="../Code/img/fundadores.jpg"   alt="Fundadores" style="width: 200px;height: 400px;"> 
-            
-            <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Ipsam necessitatibus odio quisquam reprehenderit quas, 
-                obcaecati, harum hic molestiae sequi ipsa nihil id tempore explicabo 
-                libero vero quia asperiores similique dicta.
-            </p>
-            <div class="btn-home">
-                <a href="#" class="btn">Saber mas</a>
-                <a href="#" class="btn">Saber mas</a>
-
-            </div>
-
-
-        </header>
-        <section class="content sau">
-                <h2 class="title">Productos</h2>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
-                    Officia cum voluptatibus aliquam sapiente ipsum? 
-                    Laboriosam explicabo voluptate, ad, esse id consequatur 
-                    incidunt soluta commodi officia vitae qui, quibusdam 
-                    blanditiis porro!</p>
-
-                <div class="box-container">
-                    <div class="box">
-                        <img src="../Code/img/comida.jpg"  alt="comida" style="width: 200px;height: 240px;">
-                        <h3>Planes Nutricionales</h3>
-                        <p></p>
-
-                    </div>
-                    <div class="box">
-                        <img src="../Code/img/ejercicio.jpg"  alt="ejercicio" style="width: 200px;height: 240px;">
-                        <h3>Entrenamiento</h3>
-                        <p></p>
-
-                    </div>
-                    <div class="box">
-                        <img src="../Code/img/catering.jpg"  alt="catering" style="width: 200px;height: 240px;">
-                        <h3>Catering Nutricional</h3>
-                        <p></p>
-
-                    </div>
-
-                </div>
-        </section>
-        <section class="content about">
-            <h2 class="title">Nosotros</h2>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing 
-                elit. Iure necessitatibus delectus maxime rerum dolore! Modi cupiditate rerum cum, eius, 
-                distinctio harum sit explicabo, unde iure aliquam ea.
-                Non, ut ad!</p>
-            <a href="#" class="btn">Saber mas</a>
-        </section>
-
-        <section class="content price">
-            <article class="contain">
-                <h2 class="title">Precio</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing 
-                    elit. Iure necessitatibus delectus maxime rerum dolore! Modi cupiditate rerum cum, eius, 
-                    distinctio harum sit explicabo, unde iure aliquam ea.
-                    Non, ut ad!</p>
-                    <a href="#" class="btn">Saber precio</a>
-
-            </article>
-
-        </section>
-        <section class="content contact">
-            <h2 class="title">Contacto</h2>
-            <p>6168499489</p>
-            
-            <figure class="map">
-                <img src="../Code/img/mapa.png" height="220px" width="100%" alt="mapa">
-            </figure>
-
-        </section>-->
     </body>
 </html>
