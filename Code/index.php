8a6b4e89cd3257b415854dc9c6acbc48be7b9b0a
CesarGarzonJametti <cagarzon3@espe.edu.ec>
2023-01-08 17:08:34 -0500

    Modify

--- Code/index.php
+++ Code/index.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <link rel="stylesheet" href="../css/index.css" type="text/css">
-    
-    <title>Saga Nutrition</title>
+    <link rel="stylesheet" href="../Code/css/principal.css" type="text/css" media="screen">
+
+    <title>Document</title>
 </head>
 <body>
     
@@ -17,7 +17,7 @@
         
         <div class="logo">
             
-            <img src="logo2.jpg" alt="logo2" style="width: 100;height: 70px;" >
+            <img src="../Code/img/logo2.jpg" alt="logo2" style="width: 100;height: 70px;" >
 
         </div>
         <nav class="navbar">
@@ -34,7 +34,7 @@
     </div>
     <header class="content header">
         <h2 class="title">Inicio</h2>
-        <img src="Fundadores.jpg"   alt="Fundadores" style="width: 200px;height: 400px;"> 
+        <img src="../Code/img/fundadores.jpg"   alt="Fundadores" style="width: 200px;height: 400px;"> 
         
         <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -60,19 +60,19 @@
 
             <div class="box-container">
                 <div class="box">
-                    <img src="comida.jpg"  alt="comida" style="width: 200px;height: 240px;">
+                    <img src="../Code/img/comida.jpg"  alt="comida" style="width: 200px;height: 240px;">
                     <h3>Planes Nutricionales</h3>
                     <p></p>
 
                 </div>
                 <div class="box">
-                    <img src="ejercicio.jpg"  alt="ejercicio" style="width: 200px;height: 240px;">
+                    <img src="../Code/img/ejercicio.jpg"  alt="ejercicio" style="width: 200px;height: 240px;">
                     <h3>Entrenamiento</h3>
                     <p></p>
 
                 </div>
                 <div class="box">
-                    <img src="catering.jpg"  alt="catering" style="width: 200px;height: 240px;">
+                    <img src="../Code/img/catering.jpg"  alt="catering" style="width: 200px;height: 240px;">
                     <h3>Catering Nutricional</h3>
                     <p></p>
 
@@ -106,7 +106,7 @@
         <p>6168499489</p>
         
         <figure class="map">
-            <img src="img/mapa.png" height="220px" width="100%" alt="mapa">
+            <img src="../Code/img/mapa.png" height="220px" width="100%" alt="mapa">
         </figure>
 
     </section>
