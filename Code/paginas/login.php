0a06629046fd3f069e0d851d904399bf73907657
CesarGarzonJametti <cagarzon3@espe.edu.ec>
2023-01-08 14:23:14 -0500

    Modify login.php

--- Code/paginas/login.php
+++ Code/paginas/login.php
@@ -10,15 +10,11 @@
 </head>
 <body>
   <section class="form-register">
-    <h4> Registro</h4>
+    <h4>Login</h4>
     <form method="post">
-        <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su Nombre">
-        <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Ingrese su Apellido">
-        <input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese su Correo">
-        <input class="controls" type="password" name="correo" id="correo" placeholder="Ingrese su Contraseña">
-
-        <input class="botons" type="submit" value="Registrar">
-        <p><a href="#">¿Ya tengo Cuenta?</a></p>
+        <input type="text" class="controls" name="correo" placeholder="Correo electrónico">
+        <input type="password" class="controls" name="password" placeholder="Contraseña">
+        <input type="submit" class="botons" name="bt_iniciar_sesion" value="Iniciar Sesión">
     </form>
   </section>
 </body>
