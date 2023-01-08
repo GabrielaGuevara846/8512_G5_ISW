43e3c9ca6f5fdddc165636f6c8620c7856cf9f28
EstebanGraciaX <exgracia@espe.edu.ec>
2023-01-08 16:27:02 -0500

    update code

--- Code/index.php
+++ Code/index.php
@@ -1,30 +1,40 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
-    <link rel="stylesheet" href="../Code/css/style.css">
-
-    <title>Saga Nutrition</title>
+     <link rel="stylesheet" href="saga.css">
+    <title>Document</title>
 </head>
 <body>
+    
+    
+
     <div class="head">
+        
         <div class="logo">
-            <a href="#">logo</a>
+            
+            <img src="logo2.jpg" alt="logo2" style="width: 100;height: 70px;" >
+
         </div>
         <nav class="navbar">
             <a href="#">Inicio</a>
             <a href="#">Nosotros</a>
-            <a href="#">Contactos</a>
+            <a href="#">Contacto</a>
             <a href="#">Productos</a>
             <a href="#">Precio</a>
-        </nav>
-    </div>
+            <i class="fab fa-"></i>
 
+        </nav>
+
+
+    </div>
     <header class="content header">
         <h2 class="title">Inicio</h2>
+        <img src="Fundadores.jpg"   alt="Fundadores" style="width: 200px;height: 400px;"> 
+        
         <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit.
              Ipsam necessitatibus odio quisquam reprehenderit quas, 
@@ -36,8 +46,9 @@
             <a href="#" class="btn">Saber mas</a>
 
         </div>
+
+
     </header>
-    
     <section class="content sau">
             <h2 class="title">Productos</h2>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
@@ -48,21 +59,21 @@
 
             <div class="box-container">
                 <div class="box">
-                    <i class="fab fa-angular"></i>
-                    <h3>Lorem</h3>
-                    <p>ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <img src="comida.jpg"  alt="comida" style="width: 200px;height: 240px;">
+                    <h3>Planes Nutricionales</h3>
+                    <p></p>
 
                 </div>
                 <div class="box">
-                    <i class="fab fa-apple"></i>
-                    <h3>Lorem</h3>
-                    <p>ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <img src="ejercicio.jpg"  alt="ejercicio" style="width: 200px;height: 240px;">
+                    <h3>Entrenamiento</h3>
+                    <p></p>
 
                 </div>
                 <div class="box">
-                    <i class="fab fa-android"></i>
-                    <h3>Lorem</h3>
-                    <p>ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <img src="catering.jpg"  alt="catering" style="width: 200px;height: 240px;">
+                    <h3>Catering Nutricional</h3>
+                    <p></p>
 
                 </div>
 
@@ -94,7 +105,7 @@
         <p>6168499489</p>
         
         <figure class="map">
-            <img src="../Code/img/mapa.png" height="220px" width="100%" alt="mapa">
+            <img src="img/mapa.png" height="220px" width="100%" alt="mapa">
         </figure>
 
     </section>
