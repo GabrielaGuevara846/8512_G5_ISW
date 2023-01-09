85fbea5620a51558d58e78bba64072b9497cc3d6
CesarGarzonJametti <cagarzon3@espe.edu.ec>
2023-01-08 20:42:43 -0500

    Modify

--- Code/paginas/registro.php
+++ Code/paginas/registro.php
@@ -9,15 +9,17 @@
     <title>Registro</title>
 </head>
 <body>
-  <section class="form-register">
-    <h4> Registro</h4>
-    <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su Nombre">
-    <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Ingrese su Apellido">
-    <input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese su Correo">
-    <input class="controls" type="password" name="correo" id="correo" placeholder="Ingrese su Contraseña">
-   
-    <input class="botons" type="submit" value="Registrar">
-    <p><a href="#">¿Ya tengo Cuenta?</a></p>
+  <section class="seccion_formulario">
+    <form method="post">
+      <h4> Registro</h4>
+      <input class="controls" type="text" name="nombres" id="nombres" placeholder="Ingrese su Nombre">
+      <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Ingrese su Apellido">
+      <input class="controls" type="email" name="correo" id="correo" placeholder="Ingrese su Correo">
+      <input class="controls" type="password" name="correo" id="correo" placeholder="Ingrese su Contraseña">
+    
+      <input class="botons" type="submit" value="Registrar">
+      <p><a href="#">¿Ya tengo Cuenta?</a></p>
+    </form>
   </section>
 </body>
 </html>
