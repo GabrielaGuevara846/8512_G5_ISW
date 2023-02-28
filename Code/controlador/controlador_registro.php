4038deb72a49781926584ec36923f301d78bc506
CesarGarzonJametti <cagarzon3@espe.edu.ec>
2023-02-28 16:47:29 -0500

    Modify

---
+++ Code/controlador/controlador_registro.php
@@ -0,0 +1,33 @@
+<?php
+    session_start();
+    if(!empty($_POST["bt_registro"])) {
+        if(!empty($_POST["cedula"])
+            and !empty($_POST["nombre"]) 
+            and !empty($_POST["apellido"])
+            and !empty($_POST["correo"])
+            and !empty($_POST["password"])
+            and !empty($_POST["conf_password"])
+            and ($_POST["password"] === $_POST["conf_password"])) {
+            
+            $cedula = $_POST["cedula"];
+            $nombre = $_POST["nombre"];
+            $apellido = $_POST["apellido"];
+            $correo = $_POST["correo"];
+            $password = password_hash($_POST["password"]);
+
+            $stmt = $conn->prepare("INSERT INTO usuario (cedula_identidad,
+                                                         nombres,
+                                                         apellidos,
+                                                         correo_electronico,
+                                                         contraseña) VALUES (?, ?, ?, ?, ?)");
+            $stmt->bind_param($cedula, $nombre, $apellido, $correo, $password);
+            
+            $stmt->execute();
+
+            $conn->close();
+        }
+        else {
+            echo "Campos vacíos";
+        }
+    }
+?>
--- Code/paginas/registro.php
+++ Code/paginas/registro.php
@@ -12,20 +12,21 @@
   <section class="seccion_formulario">
     <form method="post">
       <h4> Registro</h4>
-      <input class="controls" type="text" name="nombres" id="nombres" placeholder="Cédula de identidad">
-      <input class="controls" type="text" name="nombres" id="nombres" placeholder="Nombres">
-      <input class="controls" type="text" name="apellidos" id="apellidos" placeholder="Apellidos">
-      <input class="controls" type="email" name="correo" id="correo" placeholder="Correo electrónico">
-      <input class="controls" type="email" name="correo" id="correo" placeholder="Fecha de nacimiento">
-      <input class="controls" type="email" name="correo" id="correo" placeholder="Teléfono">
-      <input class="controls" type="email" name="correo" id="correo" placeholder="Sexo">
-      <input class="controls" type="email" name="correo" id="correo" placeholder="Enlace de ubicación">
-      <input class="controls" type="email" name="correo" id="correo" placeholder="Descripción de ubicación">
-      <input class="controls" type="password" name="correo" id="correo" placeholder="Contraseña">
-    
-      <input class="botons" type="submit" value="Registrar">
-      <p><a href="./login.php">¿Ya tengo Cuenta?</a></p>
+      <input class="controls" type="text" name="cedula" placeholder="Cédula de identidad">
+      <input class="controls" type="text" name="nombre" placeholder="Nombres">
+      <input class="controls" type="text" name="apellido" placeholder="Apellidos">
+      <input class="controls" type="email" name="correo" placeholder="Correo electrónico">
+      <input class="controls" type="password" name="password" placeholder="Contraseña">
+      <input class="controls" type="password" name="conf_password" placeholder="Confirmar contraseña">
+      <input class="botons" type="submit" name="bt_registro" value="Registrar">
     </form>
+
+    <p><a href="./login.php">¿Ya tengo Cuenta?</a></p>
+
+    <?php
+        include "../modelo/conexion.php";
+        include "../controlador/controlador_registro.php";
+    ?>
   </section>
 </body>
 </html>
